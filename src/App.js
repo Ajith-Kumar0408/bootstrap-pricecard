4c754e1d65e9ff714c9f6544aba31daef501eafd
Ajith-Kumar0408 <ajithak0093@gmail.com>
2023-01-10 12:47:36 +0530

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Cards } from './Cards';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List/>
+      <Table/>
     </div>
+
   );
 }
+
+
+
+function List(){
+  return <div class="Header">
+    <nav><h1>🌠Start Bootstrap</h1>
+  <li>Themes></li>
+  <li>Templates></li>
+  <li>Forms</li>
+  <li>Bundle></li>
+  <li>Resources></li></nav></div>;
+}
+
+function Table(){
+  const pricecards = [
+      {
+        plan: "FREE",
+        price: "0",
+        features:
+          [
+            {
+              usersHighlight: false,
+              title: "Single User"
+            },
+            {
+              title: "5GB Storage"
+            },
+            {
+              title: "Unlimited Public Projects"
+            },
+            {
+              title: "Community Access"
+            },
+            {
+              listType: true,
+              title: "Unlimited Private Projects"
+            },
+            {
+              listType: true,
+              title: "Dedicated Phone Support"
+            },
+            {
+              listType: true,
+              title: "Free Subdomain"
+            },
+            {
+              listType: true,
+              title: "Monthly Status Reports"
+            }
+          ],
+        buttonType: "submit"
+      },
+      {
+        plan: "PLUS",
+        price: "9",
+        features:
+          [
+            {
+              usersHighlight: true,
+              title: "5 Users"
+            },
+            { 
+              title: "50GB Storage" 
+            },
+            { 
+              title: "Unlimited Public Projects"
+            },
+            { title: "Community Access" },
+            { title: "Unlimited Private Projects" },
+            { title: "Dedicated Phone Support" },
+            { title: "Free Subdomain" },
+            {
+              listType: true,
+              title: "Monthly Status Reports"
+            }
+          ],
+        buttonType: "submit"
+      },
+      {
+        plan: "PRO",
+        price: "49",
+        features:
+          [
+            {
+              usersHighlight: true,
+              title: "Unlimited Users"
+            },
+            { title: "150GB Storage" },
+            { title: "Unlimited Public Projects" },
+            { title: "Community Access" },
+            { title: "Unlimited Private Projects" },
+            { title: "Dedicated Phone Support" },
+            {
+              usersHighlight: true,
+              title: "Unlimited Free Subdomains"
+            },
+            { title: "Monthly Status Reports" }
+          ],
+        buttonType: "submit"
+      }
+    ]
+    return (
+      <div className="container py-5">
+        <div className="row">
+          {
+            pricecards.map((ele) => {
+              return <Cards ele={ele}></Cards>
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+
 
 export default App;
